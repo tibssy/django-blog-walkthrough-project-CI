13f51fe76d195a80c356b320f7ed6857c5a1eee5
tibssy <norbert.tiborcz@gmail.com>
2024-12-14 12:26:21 +0000

    Add generic post view

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 STATUS = ((0, "Draft"), (1, "Published"))
 
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -15,7 +16,6 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
-
 
     class Meta:
         ordering = ["-created_on"]
