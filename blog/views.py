c40d31f1ffc12b000849044a4431667b0b4682a5
tibssy <norbert.tiborcz@gmail.com>
2024-12-14 12:44:14 +0000

    Filter post list

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    model = Post
-    queryset = Post.objects.all()
-    template_name = "post_list.html"
+    # model = Post
+    # queryset = Post.objects.all().order_by("-created_on")
+    queryset = Post.objects.filter(status=1)
+    # template_name = "post_list.html"
