4693b0bdbf4eb64f8f7741bc7f743e8e7cbc8c88
tibssy <norbert.tiborcz@gmail.com>
2024-12-15 15:42:27 +0000

    Add templates with styles

--- codestar/settings.py
+++ codestar/settings.py
@@ -136,8 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
