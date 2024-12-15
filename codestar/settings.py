cf1a8dae4c72ee33e16c7b941682144ece82acbc
tibssy <norbert.tiborcz@gmail.com>
2024-12-15 15:49:32 +0000

    Add templates with styles

--- codestar/settings.py
+++ codestar/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
